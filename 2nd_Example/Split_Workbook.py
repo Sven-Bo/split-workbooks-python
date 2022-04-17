50f07a6af8f7550a03347cd50d61b60547237a30
Sven-Bo <jubbel3@gmx.net>
2022-04-17 17:15:41 +0200

    [Enhance] use pathlib to handle files & folders. [Bug Fix] use fullmatch

--- 2nd_Example/Split_Workbook.py
+++ 2nd_Example/Split_Workbook.py
@@ -1,12 +1,21 @@
-import pandas as pd #pip install pandas
-import os
+from pathlib import Path
 
-df = pd.read_excel('Financial_Sample.xlsx')
-column_name = 'Country'
+import pandas as pd  # pip install pandas
+
+
+# Define & create output directory
+output_dir = Path(__file__).parent / "output"
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Define Excel file path
+excel_file = Path(__file__).parent / "Financial_Sample.xlsx"
+
+df = pd.read_excel(excel_file)
+column_name = "Country"
 df[column_name] = df[column_name].str.strip().str.title()
 unique_values = df[column_name].unique()
 
 for unique_value in unique_values:
-    df_output = df[df[column_name].str.contains(unique_value)]
-    output_path = os.path.join('output', str(unique_value) + '.xlsx')
+    df_output = df[df[column_name].str.fullmatch(unique_value)]
+    output_path = output_dir / f"{unique_value}.xlsx"
     df_output.to_excel(output_path, sheet_name=unique_value[:31], index=False)
